e8aa5000345d15a3cc408351fe18e393bce935e0
0bmxa <0bmxa@users.noreply.github.com>
2019-08-15 13:14:51 +0200

    no pip; fixed module name

--- build-macOS.sh
+++ build-macOS.sh
@@ -2,33 +2,40 @@
 
 set -eo pipefail
 
-# PYTHON_VERSION='3.7'
+# Warning: Changing this does (probably) not work yet!
+PYTHON_VERSION='3.7'
+
 
 # ==============================================================================
+#   Do not edit below this line!
+# ==============================================================================
+
 
 # Create framework output dir
+rm -rf "./build/macOS"
 mkdir -p "./build/macOS"
 
 # Create executables dir to prevent installing them into the system
 mkdir -p "./executables"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# FRAMEWORK_NAME='Python'$(echo $PYTHON_VERSION | sed 's/\./_/g')
-FRAMEWORK_NAME='Python3_7'
+FRAMEWORK_NAME='Python'$(echo $PYTHON_VERSION | sed 's/\./_/g')
 
 # Make sure openssl is installed
+printf 'Installing OpenSSL via Homebrew...\n'
 brew install openssl
 
 # Make sure wa have a clean cpython
 git submodule update --init --recursive
 cd './cpython'
 git clean -fdxq
-git checkout 3.7
+git checkout "${PYTHON_VERSION}"
 
 # Set up for framework building
 ./configure \
     --enable-ipv6 \
     --with-pydebug \
+    --without-ensurepip \
     --with-openssl=$(brew --prefix openssl) \
     --enable-framework="${SCRIPT_DIR}/build/macOS" \
     --with-framework-name="${FRAMEWORK_NAME}" \
@@ -44,8 +51,9 @@ mkdir "./build/macOS/${FRAMEWORK_NAME}.framework/Modules"
 cp "./modulemaps/${FRAMEWORK_NAME}.modulemap" \
     "./build/macOS/${FRAMEWORK_NAME}.framework/Modules/module.modulemap"
 
-# Remove build files
-git clean -fdxq
+# Replace framework name in module map
+sed -i '' -E "s/__FRAMEWORK_NAME__/${FRAMEWORK_NAME}/g" \
+    "./build/macOS/${FRAMEWORK_NAME}.framework/Modules/module.modulemap"
 
 # Remove executables dir
 rm -rf "./executables"
