a2629985440378a140f215d326fd3d0c296339ab
0bmxa <0bmxa@users.noreply.github.com>
2019-08-17 16:30:10 +0200

    Only build framework (no apps, no tools); Check for OpenSSL; Clean tree

--- build-macOS.sh
+++ build-macOS.sh
@@ -11,6 +11,17 @@ PYTHON_VERSION='3.7'
 # ==============================================================================
 
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FRAMEWORK_NAME='Python'$(echo $PYTHON_VERSION | sed 's/\./_/g')
+
+# Make sure openssl is installed
+if brew list openssl 2>/dev/null >/dev/null; then
+    printf 'OpenSSL via Homebrew found.\n'
+else
+    printf 'OpenSSL via Homebrew is required. Installing OpenSSL...\n'
+    brew install openssl
+fi
+
 # Create framework output dir
 rm -rf "./build/macOS"
 mkdir -p "./build/macOS"
@@ -18,18 +29,17 @@ mkdir -p "./build/macOS"
 # Create executables dir to prevent installing them into the system
 mkdir -p "./executables"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-FRAMEWORK_NAME='Python'$(echo $PYTHON_VERSION | sed 's/\./_/g')
-
-# Make sure openssl is installed
-printf 'Installing OpenSSL via Homebrew...\n'
-brew install openssl
-
 # Make sure wa have a clean cpython
 git submodule update --init --recursive
 cd './cpython'
 git clean -fdxq
+git reset --hard HEAD
 git checkout "${PYTHON_VERSION}"
+
+# Force building the framework only (no apps or unixtools)
+sed -i '' -E \
+    's/(FRAMEWORK(ALT)?INSTALLLAST=)".+"/\1"frameworkinstallframework"/g' \
+    ./configure*
 
 # Set up for framework building
 ./configure \
@@ -43,7 +53,7 @@ git checkout "${PYTHON_VERSION}"
 
 # Build the framework
 make
-make frameworkinstall
+make frameworkinstallframework
 
 # Copy module map
 cd '..'
